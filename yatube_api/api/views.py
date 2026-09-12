d1239fe7d4079cf9be177363d0bfbe60c2ea6c31
Andrei Moskvin <andrey.mskv@gmail.com>
2026-09-13 00:10:27 +0400

    docs: add docs to api views methods, clean comments

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -12,6 +12,7 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
+        """Устанавливает текущего пользователя автором поста."""
         serializer.save(author=self.request.user)
 
 
@@ -20,11 +21,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthorOrReadOnly]
 
     def get_queryset(self):
+        """Возвращает комментарии указанного поста."""
         post_id = self.kwargs.get('post_id')
         post = get_object_or_404(Post, id=post_id)
         return Comment.objects.filter(post=post)
 
     def perform_create(self, serializer):
+        """Создаёт комментарий для указанного поста."""
         post_id = int(self.kwargs.get('post_id'))
         post = get_object_or_404(Post, id=post_id)
         serializer.save(author=self.request.user, post=post)
